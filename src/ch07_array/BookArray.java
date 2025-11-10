f043bafe59d978d32eb015ca204d9def09d3f5c9
fnzn <jdw2318@gmail.com>
2025-11-10 22:13:41 +0900

    ch07_array

---
+++ src/ch07_array/BookArray.java
@@ -0,0 +1,11 @@
+package ch07_array;
+
+public class BookArray {
+    public static void main(String[] args) {
+        Book[ ] library = new Book[5];
+
+        for(int i = 0; i<library.length; i++){
+            System.out.println(library[i]);
+        }
+    }
+}
